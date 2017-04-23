92a185eb4a855d607d0bb2035639a4e8adf1a1d1
Sven Buijsrogge <sven@aanzee.nl>
2017-04-23 17:12:13 +0200

    Added extra test to test the url parameter

--- tests/integration/ProductsTest.php
+++ tests/integration/ProductsTest.php
@@ -14,4 +14,20 @@ class ProductsTest extends TestCase
         $this->assertArrayHasKey('total', $products);
         $this->assertCount(20, $products[ 'products' ]);
     }
+
+
+    /** @test */
+    public function it_should_get_40_products()
+    {
+        $theQuestionMarkEndpoint = new Client();
+        $products = $theQuestionMarkEndpoint->getProducts(
+            [
+                'per_page' => 40
+            ]
+        );
+
+        $this->assertArrayHasKey('products', $products);
+        $this->assertArrayHasKey('total', $products);
+        $this->assertCount(40, $products[ 'products' ]);
+    }
 }
