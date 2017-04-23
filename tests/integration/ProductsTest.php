92c0122810537202462206801cfdd803284ca694
Sven Buijsrogge <sven@aanzee.nl>
2017-04-23 16:52:19 +0200

    Written first basic test

---
+++ tests/integration/ProductsTest.php
@@ -0,0 +1,13 @@
+<?php namespace SeBuDesign\TheQuestionmark\Tests;
+
+class ProductsTest extends TestCase
+{
+    /** @test */
+    public function it_should_get_20_products_by_default()
+    {
+        $theQuestionMarkEndpoint = new Client();
+        $products = $theQuestionMarkEndpoint->getProducts();
+
+        $this->assertCount(20, $products);
+    }
+}
